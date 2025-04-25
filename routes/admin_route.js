971cc9a52cf0a571ee9b8088c77d0b8053b21492
moh0x <141264101+moh0x@users.noreply.github.com>
2025-04-25 18:27:01 +0000

    Update admin_route.js

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -3,6 +3,6 @@ const { body } = require('express-validator')
 const router = express.Router()
 const adminController = require('../controller/admin_controller')
 const {verifyToken} = require('../utility/verifyToken')
-router.post('/signup',body("email").isLength({min:8,max:50}).withMessage("typr valid email"),body('password').isLength({min:8,max:50}).withMessage('type valid password'),adminController.signUp)
-router.post('/login',body("email").isNumeric().isLength({min:8,max:50}).withMessage("type valid phone number"),body('password').isLength({min:8,max:50}).withMessage('type valid password'),adminController.login)
+router.post('/signup',body("email").isEmail().isLength({min:8,max:50}).withMessage("typr valid email"),body('password').isLength({min:8,max:50}).withMessage('type valid password'),adminController.signUp)
+router.post('/login',body("email").isEmail().isLength({min:8,max:50}).withMessage("type valid phone number"),body('password').isLength({min:8,max:50}).withMessage('type valid password'),adminController.login)
 module.exports = router
