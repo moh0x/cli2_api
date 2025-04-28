91f2d9ffb2670afa49996d1796a215c85d0e8abf
moh0x <141264101+moh0x@users.noreply.github.com>
2025-04-28 09:19:13 +0000

    Update admin_controller.js

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -59,19 +59,94 @@ res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":valid['erro
  }
 
 }
+const getAdmins = async(req,res)=>{
+  const token = req.headers.token;
+  const admin = await Admin.findOne({token:token});
+  if (admin.isAdmin == true) {
+    const admins = await Admin.find({isAdmin:false}).sort({createdAt:-1});
+    res.status(200).json({"status":httpStatus.SUCCESS,"data":admins });
+  } else {
+     res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":"you don't have permission" });
+  }
+}
+const addAdmin =async(req,res)=>{
+  try {
+    const token = req.heades.token;
+    const adminTrue = await Admin.findOne({token:token})
+    if (adminTrue.isAdmin) {
+      const valid = validationResult(req)
+      if (!valid.isEmpty()) {
+        return  res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":valid['errors'][0].msg});
+      }
+      const {email,password} = req.body      
+      const admin = await Admin.findOne({email:email});
+      if (admin) {
+        return  res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":"user already exist"})
+      }
+      const salt = await bcrypt.genSalt(10)
+      const hashPassword =await bcrypt.hash(password,salt)
+      const token = jwt.sign({email:email},"token")
+      const newAdmin = new Admin({
+          email:email,
+          password:hashPassword,
+          token:token
+      })
+      await newAdmin.save()  
+            res.status(200).json({"status":httpStatus.SUCCESS,"data":newAdmin})   
+    } else {
+      res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":"you don't have permission" });
+    }  
+  } catch (error) {
+      console.log(error);
+        res.status(400).json({"status":httpStatus.ERROR,"message":"error"})  
+  }
+}
+const editAdminPassword =async(req,res)=>{
+  try {
+    const token = req.heades.token;
+    const adminTrue = await Admin.findOne({token:token})
+    if (adminTrue.isAdmin) {
+      const valid = validationResult(req)
+      if (!valid.isEmpty()) {
+        return  res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":valid['errors'][0].msg});
+      }
+      const {_id,password} = req.body      
+      const admin = await Admin.findById(_id);
+      if (!admin) {
+        return  res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":"user not exist"})
+      }
+      const salt = await bcrypt.genSalt(10)
+      const hashPassword =await bcrypt.hash(password,salt)
+      const newAdmin = await Admin.findByIdAndUpdate(_id,{
+         $set:{
+          password:hashPassword
+         }
+      })
+      await newAdmin.save()  
+            res.status(200).json({"status":httpStatus.SUCCESS,"data":newAdmin})   
+    } else {
+      res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":"you don't have permission" });
+    }  
+  } catch (error) {
+      console.log(error);
+        res.status(400).json({"status":httpStatus.ERROR,"message":"error"})  
+  }
+}
+const deleteAdmin = async (req,res)=>{
+  try {
+    const token = req.headers.token;
+    const adminTrue = await Admin.findOne({token:token})
+      if (adminTrue.isAdmin) {
+       const {_id} = req.body 
+       await Admin.findByIdAndDelete(_id); 
+       res.status(200).json({"status":httpStatus.SUCCESS,"data":null });
+      } else {
+        res.status(400).json({"status":httpStatus.FAIL,"data":null,"message":"you don't have permission" });
+      }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
 
 
-
-// const deleteUser = async (req,res)=>{
-//   try {
-//     const token = req.headers.token;
-//     const user = await User.findOne({token:token},{password:false})
-//        await User.findByIdAndDelete(user._id); 
-//        res.status(200).json({"status":httpStatus.SUCCESS,"data":null });
-//   } catch (error) {
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// }
-
-
-module.exports = {signUp,login}
+module.exports = {signUp,login,getAdmins,addAdmin,editAdminPassword,deleteAdmin}
