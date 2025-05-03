dfa7c01bda73ef2f931d94025969dc1f46cec0b0
moh0x <141264101+moh0x@users.noreply.github.com>
2025-05-03 06:57:36 +0000

    Create maintenance_add.js

---
+++ model/maintenance_add.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const maintenanceSchema = new mongoose.Schema(
+    {
+        userId:{
+             type: mongoose.Schema.Types.ObjectId,
+                        required: true,
+                        ref: "User",
+        },
+       fullName:{
+        type:String,
+       },
+       typeOfMaintenance:{
+        type:String,
+        enum: ['كهرباء', 'سباكة', 'نجارة'],
+       },
+       details:{
+        type:String
+       }
+    },
+    { timestamps: true }
+);
+
+const Maintenance = mongoose.model("Maintenance", maintenanceSchema);
+module.exports = {Maintenance}
